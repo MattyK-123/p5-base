ab5c6348884575e688fed32a55d9aec4ee451de0
Matthew <mattkelly048@gmail.com>
2025-01-08 14:21:04 +0200

    Initial commit

--- src/sketch.js
+++ src/sketch.js
@@ -4,12 +4,11 @@ function setup() {
 
 function draw() {
   background(0x1a, 0x1a, 0x1a);
-  
+
   textAlign(CENTER, CENTER);
   fill(255);
   textSize(64);
-  text('p5-base', windowWidth / 2, windowHeight / 2);
-
+  text("p5-base", windowWidth / 2, windowHeight / 2);
 }
 
 function windowResized() {
