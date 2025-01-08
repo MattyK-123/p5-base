c93093e8cdaccf05a3ceece96b251852ac81e807
Matthew <mattkelly048@gmail.com>
2025-01-08 14:01:03 +0200

    Initial commit

---
+++ src/sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0x1A, 0x1A, 0x1A);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
